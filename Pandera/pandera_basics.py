6c7909ee6da29f520b21222ee9ebad3709659476
ahmeterenokur9 <ahmeterenokur999@gmail.com>
2025-06-30 18:00:41 +0300

    Update pandera_basics.py

--- Pandera/pandera_basics.py
+++ Pandera/pandera_basics.py
@@ -1,2 +1,28 @@
-# installing pandera 
-# pip install 'pandera[pandas]'
+import pandas as pd
+import pandera as pa
+from pandera import Check
+
+schema = pa.DataFrameSchema({
+    "column1": pa.Column(int, Check.ge(0)),
+    "column2": pa.Column(float, Check.lt(100)),
+    "column3": pa.Column(str, Check.isin(["a", "b", "c"])),
+})
+
+
+data = {
+    "column1": [1, 2, 3],
+    "column2": [10.5, 20.3, 30.1],
+    "column3": ["a", "b", "c"],
+}
+
+df = pd.DataFrame(data)
+
+try:
+    validated_df = schema.validate(df)
+    print("Validation succeeded!")
+    print(validated_df)
+except pa.errors.SchemaError as e:
+    print("Validation failed:")
+    print(e)
+
+
